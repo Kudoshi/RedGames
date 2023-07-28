864117e86a3001ea1751781991e7287bcb2ed05d
Nixon <voxell.technologies@gmail.com>
2023-07-29 02:26:38 +0800

    feat: add UXBehaviour

--- Assets/Scripts/GameWorld/UX/GameUI.cs
+++ Assets/Scripts/GameWorld/UX/GameUI.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class GameUI : MonoBehaviour
+public class GameUI : UXBehaviour
 {
+    private Label m_AsdLbl;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        InitializeDoc();
+        this.m_AsdLbl = m_Root.Q<Label>("asd");
     }
 }
