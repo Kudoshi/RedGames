a595f57ec72b6e467d55a4b65688de33528a11a3
Diviyan20 <diviyanr@gmail.com>
2023-07-30 02:53:38 +0800

    Restored Debuff Speed

---
+++ Assets/Scripts/Debuff Speed/DebuffSpeed.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuffSpeed : MonoBehaviour
+{
+    public   float slowSpeed = 1.0f; // Adjust this value as per your debuff requirement
+   public    float slowDuration = 0.2f; // Adjust this value to control how long the debuff lasts
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.CompareTag("Train"))
+        {
+            TrainMovement trainMovement = other.GetComponent<TrainMovement>();
+            if (trainMovement != null)
+            {
+               // float originalSpeed = trainMovement.GetOriginalSpeed();
+                trainMovement.ChangeTrainSpeed(-slowSpeed, slowDuration);
+                //StartCoroutine(trainMovement.RestoreTrainSpeed(originalSpeed, slowDuration));
+                Destroy(gameObject);
+                Debug.Log("Speed Decreased");
+            }
+        }
+    }
+}
