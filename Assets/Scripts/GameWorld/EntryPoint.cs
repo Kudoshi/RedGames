0b154fe1b325b364eb00afc99cbb9cf2369b8cbb
Nixon <voxell.technologies@gmail.com>
2023-07-29 16:32:07 +0800

    feat: EntryPoint now loads initial scenes

--- Assets/Scripts/GameWorld/EntryPoint.cs
+++ Assets/Scripts/GameWorld/EntryPoint.cs
@@ -1,8 +1,30 @@
 using UnityEngine;
-using Voxell.Util;
+using UnityEngine.SceneManagement;
 
 public class EntryPoint : MonoBehaviour
 {
-    [SerializeField, Scene] private string m_UXScene;
-    [SerializeField, Scene] private string m_GameScene;
+    [SerializeField, Voxell.Util.Scene] private string[] m_InitialScenes;
+
+    private void Awake()
+    {
+        int sceneCount = SceneManager.sceneCount;
+        Scene[] openedScenes = new Scene[sceneCount];
+
+        for (int s = 0; s < sceneCount; s++)
+        {
+            openedScenes[s] = SceneManager.GetSceneAt(s);
+        }
+
+        for (int i = 0; i < this.m_InitialScenes.Length; i++)
+        {
+            if (
+                !System.Array.Exists(
+                    openedScenes,
+                    (scene) => scene.name == this.m_InitialScenes[i]
+                )
+            ) {
+                SceneManager.LoadSceneAsync(this.m_InitialScenes[i], LoadSceneMode.Additive);
+            }
+        }
+    }
 }
