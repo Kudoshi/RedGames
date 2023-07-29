3560531f00767137003e52b2946b43eaa4f18e9d
Nixon <voxell.technologies@gmail.com>
2023-07-29 16:12:20 +0800

    chore: minor changes

--- Assets/Scripts/GameWorld/GridWorld/Tile.cs
+++ Assets/Scripts/GameWorld/GridWorld/Tile.cs
@@ -1,45 +1,37 @@
 using UnityEngine;
 
-using Random = Unity.Mathematics.Random;
-
 public class Tile : MonoBehaviour
 {
     public int Index => this.m_Index;
 
     private int m_Index;
-    private MeshRenderer m_MeshRenderer;
+    // private MeshRenderer m_MeshRenderer;
 
     public void Initialize(int randIndex, TileConfig[] tileConfigs)
     {
         this.m_Index = randIndex;
 
-        TileConfig config = tileConfigs[0];
-        for (int t = 1; t < tileConfigs.Length; t++)
+        int configIndex = -1;
+        for (int t = 0; t < tileConfigs.Length; t++)
         {
-            if (randIndex < tileConfigs[t].UpperBound)
+            if (randIndex <= tileConfigs[t].UpperBound)
             {
                  break;
             }
-            config = tileConfigs[t];
+            configIndex = t;
         }
+
+        if (configIndex == -1) return;
+
+        TileConfig config = tileConfigs[configIndex];
 
         Transform trans = config.Pool.GetNextObject();
         trans.gameObject.SetActive(true);
         trans.position = this.transform.position + Vector3.up;
-
-        // DEBUG: set a color based on index (for debug visualization onnly)
-        // Random rand = Random.CreateFromIndex((uint)index);
-        // MaterialPropertyBlock block = new MaterialPropertyBlock();
-
-        // block.SetColor(
-        //     "_BaseColor",
-        //     new Color(rand.NextFloat(), rand.NextFloat(), rand.NextFloat())
-        // );
-        // this.m_MeshRenderer.SetPropertyBlock(block);
     }
 
     private void Awake()
     {
-        this.m_MeshRenderer = this.GetComponent<MeshRenderer>();
+        // this.m_MeshRenderer = this.GetComponent<MeshRenderer>();
     }
 }
