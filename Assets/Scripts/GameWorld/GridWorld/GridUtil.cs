6d85347001f97a7637c8d8dcf40c468b5b94c3ae
Nixon <voxell.technologies@gmail.com>
2023-07-29 11:37:10 +0800

    feat: add grid tile generator

---
+++ Assets/Scripts/GameWorld/GridWorld/GridUtil.cs
@@ -0,0 +1,17 @@
+using Unity.Mathematics;
+
+public static class GridUtil
+{
+    public const uint PRIME1 = 73856093;
+    public const uint PRIME2 = 83492791;
+
+    /// <summary>Get a random index for a given tile position</summary>
+    public static int GetTileRandIndex(int2 tilePosition, int maxIndex)
+    {
+        // spatial hashing function for 2D
+        return (int)(
+            ((uint)tilePosition.x * PRIME1) ^
+            ((uint)tilePosition.y * PRIME2) % (uint)maxIndex
+        );
+    }
+}
