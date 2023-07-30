16775655888de18a6967b5ef239aa15cce136c61
Nixon <voxell.technologies@gmail.com>
2023-07-30 17:23:03 +0800

    feat: add QuadraticBezierUtil

--- Assets/Scripts/BezierCurveTest.cs
+++ Assets/Scripts/BezierCurveTest.cs
@@ -1,21 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class BezierCurveTest : MonoBehaviour
 {
     public Transform StartTrans;
+    public Transform ControlTrans;
     public Transform EndTrans;
 
-    // Start is called before the first frame update
-    void Start()
+    public Transform TargetTrans;
+    public Transform TargetTrans1;
+
+    [Range(0.0f, 2.0f)] public float Time;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float3 a = this.StartTrans.position;
+        float3 b = this.ControlTrans.position;
+        float3 c = this.EndTrans.position;
+
+        // this.TargetTrans.position = math.lerp(math.lerp(a, b, this.Time), math.lerp(b, c, this.Time), this.Time);
+        this.TargetTrans.position = QuadraticBezierUtil.GetPosition(a, b, c, this.Time);
+        this.TargetTrans1.position = (float3)this.TargetTrans.position + QuadraticBezierUtil.GetTangent(a, b, c, this.Time);
     }
 }
