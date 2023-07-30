4b584e3b7a570779ba75074dae700c700de51497
Nixon <voxell.technologies@gmail.com>
2023-07-30 12:28:30 +0800

    feat: implementation of persistent grid tile generation

--- Assets/Scripts/GameWorld/GridWorld/GridUtil.cs
+++ Assets/Scripts/GameWorld/GridWorld/GridUtil.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
 using Unity.Mathematics;
+using GameWorld.Util;
 
 public static class GridUtil
 {
@@ -13,5 +16,91 @@ public static class GridUtil
             (((uint)tilePosition.x * PRIME1) ^
             ((uint)tilePosition.y * PRIME2)) % (uint)maxIndex
         );
+    }
+
+    public static void UpdateTileAvailability(
+        int2 center, int2 size,
+        ref List<int> unusedTileIndices,
+        in Tile[] tiles
+    ) {
+        // clear list before adding in tile indices
+        unusedTileIndices.Clear();
+        int2 halfSize = size / 2;
+
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                int tileIndex = GetTileIndex(x, y, size);
+                Tile tile = tiles[tileIndex];
+                int2 position = (int2)mathxx.flatten_3d(
+                    tile.transform.position
+                );
+
+                if (!TileInScreen(position, center, size))
+                {
+                    Debug.Log(tileIndex);
+                    unusedTileIndices.Add(tileIndex);
+                }
+            }
+        }
+    }
+
+    public static void UpdateTiles(
+        int2 center, int2 size, LayerMask tileLayer,
+        in List<int> unusedTileIndices,
+        in Tile[] tiles
+    ) {
+        int index = 0;
+        int2 halfSize = size / 2;
+
+        Collider[] colliders = new Collider[1];
+
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                int2 position = GetTilePosition(x, y, halfSize, center);
+                float3 position3D = mathxx.unflatten_2d(position);
+
+                Physics.OverlapSphereNonAlloc(
+                    position3D, 0.3f, colliders, tileLayer
+                );
+
+                if (colliders[0] == null)
+                {
+                    int tileIndex  = unusedTileIndices[index++];
+                    Tile tile = tiles[tileIndex];
+
+                    tile.transform.position = position3D;
+                    // tile.Initialize();
+                }
+
+                colliders[0] = null;
+            }
+        }
+    }
+
+    public static bool TileInScreen(
+        int2 position, int2 center, int2 size
+    ) {
+        int2 halfSize = size / 2;
+        int2 minBound = GetTilePosition(0, 0, halfSize, center);
+        int2 maxBound = GetTilePosition(size.x, size.y, halfSize, center);
+
+        return math.all(position >= minBound) && math.all(position < maxBound);
+    }
+
+    public static int2 GetTilePosition(
+        int x, int y,
+        int2 halfSize, int2 center
+    ) {
+        int2 position = new int2(x, y);
+        return position - halfSize + center;
+    }
+
+    public static int GetTileIndex(int x, int y, int2 size)
+    {
+        return x + y * size.x;
     }
 }
--- Assets/Scripts/GameWorld/GridWorld/GridWorld.cs
+++ Assets/Scripts/GameWorld/GridWorld/GridWorld.cs
@@ -7,8 +7,12 @@ public class GridWorld : MonoBehaviour
     [SerializeField] private Pool<Tile> m_TilePool;
     [SerializeField] private int2 m_Size;
 
-    [SerializeField] private int2 m_Center;
+    [SerializeField] private int2 m_PrevCenter;
+    [SerializeField] private int2 m_CurrCenter;
     [SerializeField] private Transform m_TargetTransform;
+
+    [SerializeField] private LayerMask m_TileMask;
+
     [SerializeField] private TileConfig[] m_TileConfigs;
 
     private int2 m_HalfSize;
@@ -18,30 +22,42 @@ public class GridWorld : MonoBehaviour
         int2 currCenter = new int2((int)center.x, (int)center.z);
 
         // if target transform move out of range, regenerate tiles
-        if (!this.m_Center.Equals(currCenter))
+        if (!this.m_CurrCenter.Equals(currCenter))
         {
-            this.m_Center = currCenter;
+            this.m_PrevCenter = this.m_CurrCenter;
+            this.m_CurrCenter = currCenter;
 
             // set all as false
-            this.m_TilePool.SetAllObjectActive(false);
+            // this.m_TilePool.SetAllObjectActive(false);
 
-            for (int t = 0; t < this.m_TileConfigs.Length; t++)
-            {
-                this.m_TileConfigs[t].Pool.SetAllObjectActive(false);
-            }
+            // for (int t = 0; t < this.m_TileConfigs.Length; t++)
+            // {
+            //     this.m_TileConfigs[t].Pool.SetAllObjectActive(false);
+            // }
 
             this.GenerateTileArea();
         }
     }
 
-    private void GenerateTileArea()
+    private void GenerateTileArea(bool checkTileInScreen = true)
     {
-        for (int x = 0; x < this.m_Size.x; x++)
+        for (int y = 0; y < this.m_Size.y; y++)
         {
-            for (int y = 0; y < this.m_Size.y; y++)
+            for (int x = 0; x < this.m_Size.x; x++)
             {
                 int2 position = new int2(x, y);
-                position = position - this.m_HalfSize + this.m_Center;
+                position = position - this.m_HalfSize + this.m_CurrCenter;
+
+                if (this.CheckTileInScreen(position) && checkTileInScreen)
+                {
+                    continue;
+                }
+
+                // Collider[] colliders = Physics.OverlapSphere(
+                //     new Vector3(position.x, 0.0f, position.y), 1.0f,
+                    
+                // )
+
                 this.SpawnTile(position);
             }
         }
@@ -66,6 +82,14 @@ public class GridWorld : MonoBehaviour
         );
     }
 
+    private bool CheckTileInScreen(int2 position)
+    {
+        int2 maxBound = this.m_PrevCenter + this.m_HalfSize;
+        int2 minBound = this.m_PrevCenter - this.m_HalfSize;
+
+        return math.all(position >= minBound) && math.all(position <= maxBound);
+    }
+
     private void Start()
     {
         this.m_TilePool.Initialize(this.transform);
@@ -77,7 +101,7 @@ public class GridWorld : MonoBehaviour
         }
 
         this.UpdateCenter(this.m_TargetTransform.position);
-        this.GenerateTileArea();
+        this.GenerateTileArea(false);
     }
 
     private void Update()
